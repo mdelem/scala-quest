36842b3fdd2671dc1c4e3c67007a61fa052b78e3
martin.delemotte@gmail.com <martin.delemotte@gmail.com>
2015-10-22 16:03:17 -0500

    Project structure and licence

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := """scala-quest"""
+
+version := "0.1-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
---
+++ src/test/scala/com/github/mdelem/scalaquest/QuestionnaireSpec.scala
@@ -0,0 +1,18 @@
+package com.github.mdelem.scalaquest
+
+import org.scalatest._
+
+class QuestionnaireSpec extends FlatSpec with Matchers {
+  "A Questionnaire" should "contain one or several parts" in {
+    
+  }
+  "A Part" should "contain one or several item groups" in {
+    
+  }
+  "An Item Group" should "contain (sub) questions groups or items" in {
+    
+  }
+  "An Item" should "contain questions groups or items" in {
+    
+  }
+}
