9bccbf594dc994b82da99155cb13befee2fadb38
martin.delemotte@gmail.com <martin.delemotte@gmail.com>
2015-10-24 11:02:06 -0500

    Fixed and added tests for questionnaire-building implicits

--- src/main/scala/com/github/mdelem/scalaquest/Questionnaire.scala
+++ src/main/scala/com/github/mdelem/scalaquest/Questionnaire.scala
@@ -4,9 +4,9 @@ trait Item {
   def name: Option[String]
 }
 
-case class SimpleItem(name: Option[String] = None, proposition: String, acceptedType: String = "String") extends Item
+case class SimpleItem[T](name: Option[String] = None, proposition: String) extends Item
 
-case class ComplexItem(name: Option[String] = None, items: Seq[SimpleItem], randomized: Boolean = false) extends Item
+case class ComplexItem(name: Option[String] = None, items: Seq[SimpleItem[_]], randomized: Boolean = false) extends Item
 
 case class ItemGroup(name: Option[String] = None, items: Seq[Item], randomized: Boolean = false)
 
@@ -30,26 +30,27 @@ object ItemGroup {
 }
 
 object ComplexItem {
-  def apply(items: Seq[SimpleItem]): ComplexItem = ComplexItem(None, items)
-  def apply(items: Seq[SimpleItem], randomized: Boolean): ComplexItem = ComplexItem(None, items, randomized)
+  def apply(items: Seq[SimpleItem[_]]): ComplexItem = ComplexItem(None, items)
+  def apply(items: Seq[SimpleItem[_]], randomized: Boolean): ComplexItem = ComplexItem(None, items, randomized)
 }
 
 object SimpleItem {
-  def apply(proposition: String): SimpleItem = SimpleItem(None, proposition)
-  def apply(proposition: String, acceptedType: String): SimpleItem = SimpleItem(None, proposition, acceptedType)
+  def apply[T](proposition: String): SimpleItem[T] = SimpleItem(None, proposition)
+//  def apply[T](proposition: String, acceptedType: String): SimpleItem[T] = SimpleItem(None, proposition)
 }
 
 object Implicits {
   implicit def stringToSomeString(s: String) = Some(s)
 
   implicit def itemToItemGroup(i:Item) = ItemGroup(Seq(i))
+  implicit def itemToSeqItemGroup(i:Item) = Seq(ItemGroup(Seq(i)))
   
   implicit def partAsSeq(p : Part) = Seq(p)
   implicit class PartAsSeq(p : Part) {
     def ~(next : Part) : Seq[Part] = Seq(p, next)
   }
   implicit class PartSeq(p : Seq[Part]) {
-    def ~(next : Part) : Seq[Part] = p ++ next
+    def ~(next : Part) : Seq[Part] = p :+ next
   }
   
   implicit def itemGroupAsSeq(p : ItemGroup) = Seq(p)
@@ -57,7 +58,7 @@ object Implicits {
     def ~(next : ItemGroup) : Seq[ItemGroup] = Seq(p, next)
   }
   implicit class ItemGroupSeq(p : Seq[ItemGroup]) {
-    def ~(next : ItemGroup) : Seq[ItemGroup] = p ++ next
+    def ~(next : ItemGroup) : Seq[ItemGroup] = p :+ next
   }
   
   implicit def itemAsSeq(i : Item) = Seq(i)
@@ -65,14 +66,24 @@ object Implicits {
     def ~(next : Item) : Seq[Item] = Seq(i, next)
   }
   implicit class ItemSeq(i : Seq[Item]) {
-    def ~(next : Item) : Seq[Item] = i ++ next
+    def ~(next : Item) : Seq[Item] = i :+ next
+    def ~(next : ItemGroup) : Seq[ItemGroup] = i.map(j=>ItemGroup(Seq(j))) :+ next
   }
   
-  implicit def simpleItemAsSeq(i : SimpleItem) = Seq(i)
-  implicit class SimpleItemAsSeq(i : SimpleItem) {
-    def ~(next : SimpleItem) : Seq[SimpleItem] = Seq(i, next)
+  implicit def simpleItemAsSeq[T](i : SimpleItem[T]) = Seq(i)
+  implicit class SimpleItemAsSeq[T](i : SimpleItem[T]) {
+    def ~[U](next : SimpleItem[U]) : Seq[SimpleItem[_]] = Seq(i, next)
+    def ~(next : ItemGroup) : Seq[ItemGroup] = Seq(ItemGroup(Seq(i)), next)
+    def ~(next : ComplexItem) : Seq[Item] = Seq(i, next)
   }
-  implicit class SimpleItemSeq(i : Seq[SimpleItem]) {
-    def ~(next : SimpleItem) : Seq[SimpleItem] = i ++ next
+  implicit class SimpleItemSeq(i : Seq[SimpleItem[_]]) {
+    def ~[U](next : SimpleItem[U]) : Seq[SimpleItem[_]] = i :+ next
+    def ~(next : ItemGroup) : Seq[ItemGroup] = i.map(j=>ItemGroup(Seq(j))) :+ next
+    def ~(next : ComplexItem) : Seq[Item] = i :+ next
+  }
+  
+  implicit class ComplexItemAsSeq(i : ComplexItem) {
+    def ~(next : ItemGroup) : Seq[ItemGroup] = Seq(ItemGroup(Seq(i)), next)
+    def ~(next : Item) : Seq[Item] = Seq(i, next)
   }
 }
--- src/test/scala/com/github/mdelem/scalaquest/QuestionnaireSpec.scala
+++ src/test/scala/com/github/mdelem/scalaquest/QuestionnaireSpec.scala
@@ -4,21 +4,18 @@ import org.scalatest._
 import com.github.mdelem.scalaquest.Implicits._
 
 class QuestionnaireSpec extends FlatSpec with Matchers {
+
   val q = Questionnaire(
     name = "q1",
     Part(
       ItemGroup(
-        SimpleItem("Age", "Integer") ~
-        SimpleItem("Gender")
-      ) ~
-      SimpleItem("I like chocolate", "1-10 Scale") ~
-      SimpleItem("I like vanilla", "1-10 Scale") ~
-      ComplexItem(
-        SimpleItem("I prefer chocolate", "Boolean") ~
-        SimpleItem("I prefer vanilla", "Boolean")
-      )
-    )
-  )
+        SimpleItem[Int]("d1", "Age") ~
+          SimpleItem[String]("d2", "Gender")) ~
+        SimpleItem[Int]("I like chocolate") ~
+        SimpleItem[Int]("I like vanilla") ~
+        ComplexItem(
+          SimpleItem[Boolean]("I prefer chocolate") ~
+            SimpleItem[Boolean]("I prefer vanilla"))))
 
   "A Questionnaire" should "contains parts" in {
     q.parts.size should be(1)
@@ -30,15 +27,46 @@ class QuestionnaireSpec extends FlatSpec with Matchers {
     q.parts.head.groups.head.items.size should be(2)
   }
   "An Item" should "is either a simple item or a complex item" in {
-    q.parts.head.groups.drop(1).head.items.head shouldBe an[SimpleItem]
+    q.parts.head.groups.drop(1).head.items.head shouldBe an[SimpleItem[_]]
     q.parts.head.groups.drop(3).head.items.head shouldBe an[ComplexItem]
   }
   "A Complex Item" should "contains simple items" in {
-    q.parts.head.groups.drop(3).head.items.head.asInstanceOf[ComplexItem].items.head shouldBe an[SimpleItem]
+    q.parts.head.groups.drop(3).head.items.head.asInstanceOf[ComplexItem].items.head shouldBe an[SimpleItem[_]]
   }
   "A Simple Item" should "contains a proposition and specifies a response type" in {
     val i = q.parts.head.groups.drop(1).head.items.head
-    i.asInstanceOf[SimpleItem].proposition should be("I like chocolate")
-    i.asInstanceOf[SimpleItem].acceptedType should be("1-10 Scale")
+    i.asInstanceOf[SimpleItem[_]].proposition should be("I like chocolate")
   }
+
+  // Building a complex questionnaire to check if the implicits 
+  // work as expected and there are no compile time errors
+  Questionnaire(
+    name = "q2",
+    Part(
+      SimpleItem[Int]("I like chocolate") ~
+        SimpleItem[Int]("I like vanilla") ~
+        ComplexItem(
+          SimpleItem[Boolean]("I prefer chocolate") ~
+            SimpleItem[Boolean]("I prefer vanilla"))) ~
+      Part(
+        ComplexItem(
+          SimpleItem[Boolean]("I prefer chocolate") ~
+            SimpleItem[Boolean]("I prefer vanilla")) ~
+          SimpleItem[Int]("I like chocolate") ~
+          SimpleItem[Int]("I like vanilla") ~
+          ItemGroup(
+            SimpleItem[Int]("d1", "Age") ~
+              ComplexItem(
+                SimpleItem[Boolean]("I prefer vanilla"))) ~
+            ItemGroup(
+              ComplexItem(
+                SimpleItem[Boolean]("I prefer vanilla")) ~
+                SimpleItem[Int]("d1", "Age"))) ~
+        Part(
+          SimpleItem[Int]("I like chocolate")) ~
+          Part(
+            ItemGroup(
+              ComplexItem(
+                SimpleItem[Boolean]("I prefer vanilla")))))
+
 }
