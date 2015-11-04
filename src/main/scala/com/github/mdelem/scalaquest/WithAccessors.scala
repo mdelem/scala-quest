45b03db4c8e5ff6c0222fca9fe418a05b7b50e91
martin.delemotte@gmail.com <martin.delemotte@gmail.com>
2015-11-04 05:55:02 -0500

    Added check so that @WithAccessors can only be applied to Questionnaire declarations

--- src/main/scala/com/github/mdelem/scalaquest/WithAccessors.scala
+++ src/main/scala/com/github/mdelem/scalaquest/WithAccessors.scala
@@ -13,27 +13,20 @@ class WithAccessors extends StaticAnnotation {
 object WithAccessorsMacro {
   def impl(c: Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
     import c.universe._
-    def findPartNames(t: Tree): Seq[String] = {
-      val maybePartName = t match {
-        case Apply(Ident(TermName("Part")), (AssignOrNamedArg(Ident(TermName("name")),
-          Literal(Constant(partName: String))) :: _)) => Some(partName)
-        case _ => None
-      }
 
-      (maybePartName ++ t.children.flatMap(findPartNames(_))).toSeq
-    }
+    val inputAsTree = annottees.head.tree
+    inputAsTree match {
+      case input @ ValDef(_, _, _, Apply(Ident(TermName("Questionnaire")), _)) =>
+        val wrapperName = s"Questionnaire${input.name.encodedName.toString.capitalize}WithAccessors"
 
-    val input = annottees.head.tree.asInstanceOf[ValDef]
-    val wrapperName = s"Questionnaire${input.name.encodedName}WithAccessors"
+        val wrappedCode = ValDef(Modifiers(), input.name, Ident(TypeName(wrapperName)),
+          input.children(1))
 
-    val wrappedCode = ValDef(Modifiers(), input.name, Ident(TypeName(wrapperName)),
-      input.children(1))
-
-    val accessors = findPartNames(input)
-      .map(partName => s"""def _$partName = this.part("$partName")""")
-      .mkString("\n")
-    val wrapperDef = c.parse(
-      s"""
+        val accessors = findPartNames(c)(input)
+          .map(partName => s"""def _$partName = this.part("$partName")""")
+          .mkString("\n")
+        val wrapperDef = c.parse(
+          s"""
       class $wrapperName(name: Option[String] = None, randomized: Boolean = false, parts: Seq[Part])
           extends Questionnaire(name, randomized, parts) {
         $accessors
@@ -46,8 +39,24 @@ object WithAccessorsMacro {
       import $wrapperName._
       """)
 
-    val output = Block(wrapperDef.children :+ wrappedCode, Literal(Constant(())))
-    c.Expr[Any](output)
+        val output = Block(wrapperDef.children :+ wrappedCode, Literal(Constant(())))
+        c.Expr[Any](output)
+      case _ =>
+        c.abort(c.enclosingPosition, "Annotation can only be applied to Questionnaire declarations")
+    }
+
+  }
+
+  def findPartNames(c: Context)(t: c.Tree): Seq[String] = {
+    import c.universe._
+
+    val maybePartName = t match {
+      case Apply(Ident(TermName("Part")), (AssignOrNamedArg(Ident(TermName("name")),
+        Literal(Constant(partName: String))) :: _)) => Some(partName)
+      case _ => None
+    }
+
+    (maybePartName ++ t.children.flatMap(findPartNames(c)(_))).toSeq
   }
 
 }
